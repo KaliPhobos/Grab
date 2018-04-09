f77dc649e9981613e4e8c3a03abbf31d5e36ecd5
p2 <p2abc@aol.com>
2018-04-09 20:00:05 +0200

    v00s02

--- main.java
+++ main.java
@@ -1,4 +1,4 @@
-package v00s01;
+package v00s02;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -6,10 +6,29 @@ import java.util.List;
 
 public class main {
 	public static void main(String[] args) {
-		String[] Endings = {"jpg", "png", "bmp", "doc", "docx", "ppt", "pptx", "txt", "mp4", "mkv", "avi", "exe", "zip", "rar", "7zip", "dll"};
+		String[][] Types = {						// ending, category, relevance, quantity
+				{"jpg", "Image", "90", "0"},
+				{"png", "Image", "100", "0"},
+				{"bmp", "Image", "20", "0"},
+				{"doc", "Document", "80", "0"},
+				{"docx", "Document", "80", "0"},
+				{"ppt", "Document", "70", "0"},
+				{"pptx", "Document", "70", "0"},
+				{"txt", "Document", "90", "0"},
+				{"mp4", "Video", "60", "0"},
+				{"mkv", "Video", "60", "0"},
+				{"avi", "Video", "50", "0"},
+				{"exe", "Executable", "30", "0"},
+				{"zip", "Archive", "60", "0"},
+				{"rar", "Archive", "60", "0"},
+				{"7zip", "Archive", "60", "0"},
+				{"dll", "Binary", "10", "0"}
+				};
+		
+		/*String[] Endings = {"jpg", "png", "bmp", "doc", "docx", "ppt", "pptx", "txt", "mp4", "mkv", "avi", "exe", "zip", "rar", "7zip", "dll"};
 		String[] Category = {"Image", "Image", "Image", "Document", "Document", "Document", "Document", "Document", "Video", "Video", "Video", "Executable", "Archive", "Archive", "Archive", "Binary"};
 		int[] Priority = {90, 100, 20, 80, 80, 70, 70, 90, 60, 60, 50, 30, 60, 60, 60, 10};
-		int[] Counter = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		int[] Counter = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};*/
 		
 		String path = "C:\\xampp";
 		File folder = new File(path);
@@ -17,24 +36,23 @@ public class main {
 		List<File> allFiles = new ArrayList<File>();
 
 		    for (int i = 0; i < listOfFiles.length; i++) {
-		    	if (listOfFiles[i].isDirectory()) {
+		    	if (listOfFiles[i].isDirectory()) {									// Enter subfolder
 		    		System.out.println(path+"\\"+listOfFiles[i].getName());
 		    		File[] temp = new File(path+"\\"+listOfFiles[i].getName()).listFiles();
 				    for (int j = 0; j < temp.length; j++) {
-				    	//listOfFiles[listOfFiles.length] = temp[j];
-				    	allFiles.add(temp[j]);
+				    	allFiles.add(temp[j]);										// Add file in folder to list
 				    }
 		    	} else {
-			    	allFiles.add(listOfFiles[i]);
+			    	allFiles.add(listOfFiles[i]);									// Add file to list
 		    	}
 		    }
 		    for (int i = 0; i < allFiles.size()-1; i++) {
 		        System.out.println("File " + allFiles.get(i).getName());
 		        String[] temp = allFiles.get(i).getName().split("\\.");
 		        String Ending = temp[temp.length-1];
-		        for(int _x = 0;_x<Endings.length;_x++) {
-		        	if(Ending.equals(Endings[_x])) {
-		        		Counter[_x]++;
+		        for(int _x = 0;_x<Types.length;_x++) {
+		        	if(Ending.equals(Types[_x][0])) {
+		        		Types[_x][3] = (Integer.parseInt(Types[_x][3])+1)+"";		// Count 1 up
 		        	}
 		        }
 		    }
@@ -43,13 +61,14 @@ public class main {
 		    
 		    int[] Importances = new int[allFiles.size()];
 		    for (int i = 0; i < allFiles.size(); i++) {
+		    	//System.out.println(i);
 		    	String[] temp = allFiles.get(i).getName().split("\\.");
 			    String Ending = temp[temp.length-1];
-				for(int _x = 0;_x<Endings.length;_x++) {
-					if(Ending.equals(Endings[_x])) {
-			       		int Importance = (Priority[_x]*Priority[_x])/Counter[_x];
+				for(int _x = 0;_x<Types.length;_x++) {
+					if(Ending.equals(Types[_x][0])) {
+			       		int Importance = (Integer.parseInt(Types[_x][2])*Integer.parseInt(Types[_x][2]))/Integer.parseInt(Types[_x][3]);
 			       		Importances[i] = Importance;
-			       		System.out.println("File " + allFiles.get(i).getName() + " is of type " + Category[_x] + "(priority "+Priority[_x]+"%) and got the individual Importance of " + Importance);
+			       		System.out.println("File " + allFiles.get(i).getName() + " is of type " + Types[_x][1] + "(priority "+Types[_x][2]+"%) and got the individual Importance of " + Importance);
 			       	}
 		    	}
 		    }
